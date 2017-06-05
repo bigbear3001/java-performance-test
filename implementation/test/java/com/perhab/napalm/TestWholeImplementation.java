48b06e0bb0b9245a0493385873f18c61ba367008
Andreas Perhab <bigbear.ap@gmail.com>
2017-06-05 11:48:29 +0200

    added module project in parent dir, add unittest as method of execution

---
+++ implementation/test/java/com/perhab/napalm/TestWholeImplementation.java
@@ -0,0 +1,11 @@
+package com.perhab.napalm;
+
+import org.junit.Test;
+
+public class TestWholeImplementation {
+
+    @Test
+    public void testEverything() {
+        Runner.main(new String[]{});
+    }
+}
