4566f9f54d18fc40971ab88111df75c1d0ec1f58
Andreas Perhab <bigbear.ap@gmail.com>
2017-08-04 18:54:35 +0200

    styled html output with bootstrap (loaded via CDN)

--- api/src/main/java/com/perhab/napalm/HtmlFileOutPrinter.java
+++ api/src/main/java/com/perhab/napalm/HtmlFileOutPrinter.java
@@ -21,6 +21,12 @@ public class HtmlFileOutPrinter implements Printer {
 
     private final File file;
 
+    private static final String BOOTSTRAP =
+            "<!-- Compiled and minified CSS -->\n" +
+            "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\n" +
+            "<!-- Compiled and minified JavaScript -->\n" +
+            "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\n";
+
     public HtmlFileOutPrinter(String filename) {
         file = new File(filename);
     }
@@ -36,17 +42,27 @@ public class HtmlFileOutPrinter implements Printer {
             FileOutputStream fos = new FileOutputStream(file);
             @Cleanup
             OutputStreamWriter writer = new OutputStreamWriter(fos);
-            writer.append("<html><head><title>Performance Report</title></head><body>");
+            writer.append("<html lang=\"en\">\n<head>\n<title>Performance Report</title>\n" + BOOTSTRAP + "</head>\n<body>\n");
+            writer.append("<div class=\"container\">\n");
             writer.append("<h1>Performance Report</h1>\n");
-            writer.append("<p>Created at " + new SimpleDateFormat("dd.MM.yyyy").format(new Date()) + "<br />\n");
-            writer.append("on " + getSystemInformation() + "</p>\n");
-            writer.append("<table><tr><th>Name</th><th>1</th><th>100</th><th>10000</th><th>1000000</th></tr>\n");
+            writer.append("<div class=\"jumbotron\">Created at " + new SimpleDateFormat("dd.MM.yyyy").format(new Date()) + "<br />\n");
+            writer.append("on " + getSystemInformation() + "</div>\n");
             Class<?> lastImplementedInterface = null;
             for (Result result : results) {
                 Class<?> implementedInterface = result.getStatement().getGroup().getImplementedInterface();
                 if (implementedInterface != lastImplementedInterface) {
-                    writer.append("<tr><td colspan=\"5\"><b>" + implementedInterface.getCanonicalName() + "</b></td></tr>\n");
+                    if (lastImplementedInterface != null) {
+                        writer.append("</tbody></table></div>");
+                    }
+                    writer.append("<div class=\"row\"><h2>" + implementedInterface.getCanonicalName() + "</h2>\n");
                     lastImplementedInterface = implementedInterface;
+                    writer.append("<table class=\"table\"><thead><tr>" +
+                            "<th>Name</th>" +
+                            "<th class=\"text-right\"><span title=\"time needed for one invocation\">1</span></th>" +
+                            "<th class=\"text-right\"><span title=\"time needed for one hundred invocations\">100</span></th>" +
+                            "<th class=\"text-right\"><span title=\"time needed for ten thousand invocations\">10000</span></th>" +
+                            "<th class=\"text-right\"><span title=\"time needed tor one million invocations\">1000000</span></th>" +
+                            "</tr></thead><tbody>\n");
                 }
                 writer.append("<tr><td>");
                 writer.append(result.getStatement().toString());
@@ -56,14 +72,14 @@ public class HtmlFileOutPrinter implements Printer {
                     if (startTime == STARTTIME_NOT_SET) {
                         startTime = time;
                     } else {
-                        writer.append("</td><td>");
+                        writer.append("</td><td class=\"text-right\">");
                         writer.append((time - startTime) + "");
                         writer.append("ms");
                     }
                 }
                 writer.append("</td></tr>\n");
             }
-            writer.append("</table></body></html>");
+            writer.append("</tbody></table></div></div></body></html>");
             writer.flush();
 
         } catch (java.io.IOException e) {
