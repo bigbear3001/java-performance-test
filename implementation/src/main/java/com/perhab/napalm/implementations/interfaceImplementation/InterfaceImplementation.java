996e8b8d56e1096c6dc8b0f5825f4c96821e4b26
Andreas Perhab <bigbear.ap@gmail.com>
2017-12-05 21:22:20 +0100

    use more iterations for interface implementations

--- implementation/src/main/java/com/perhab/napalm/implementations/interfaceImplementation/InterfaceImplementation.java
+++ implementation/src/main/java/com/perhab/napalm/implementations/interfaceImplementation/InterfaceImplementation.java
@@ -10,7 +10,7 @@ public interface InterfaceImplementation {
 			// Array length needs to be a multiple of 16 currently (not all classes have length checks)
 			@Parameter("1"),
 			@Parameter("2")
-	})
+	}, iterations = {1, 1000, 1000000, 10000000})
 	@Description("Tests the performance of implementing an interface.")
 	Integer add(Integer a, Integer b) throws Exception;
 }
